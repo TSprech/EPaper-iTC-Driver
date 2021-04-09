da425967096c58a36db06a9ea6b3993549474e83
TSprech <trey.sprecher@gmail.com>
2021-04-09 10:05:33 -0400

    Wrote Pixel Method
    Figured out how to (hopefully, untested as I do not have the hardware yet) draw pixels without rotation. Todo: Add rotation support as the initial rotation of the screens is a little odd for the higher aspect ratio screens.

--- lib/iTC/PDi_iTC.cpp
+++ lib/iTC/PDi_iTC.cpp
@@ -1,15 +1,16 @@
 #include "PDi_iTC.hpp"
 
 void ITC::Initialize() {
+  // Set pin directions
   pinMode(chip_select_pin_, OUTPUT);
   pinMode(command_data_pin_, OUTPUT);
   pinMode(reset_pin_, OUTPUT);
 
   // Start the SPI bus
   SPI.begin();
-  SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
+  SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0)); // Speed this up once tested, max is 10MHz
 
-  // The reset pin must be toggled with a specific timing
+  // The reset pin must be toggled with a specific timing, as detailed in the datasheet
   delay(5);
   ResetPin(1);
   delay(5);
@@ -29,34 +30,44 @@ void ITC::Temperature(uint8_t temperature) {
 }
 
 void ITC::Pixel(uint16_t x, uint16_t y, Colors color) {
+  if ((x >= width_) || (y >= height_)) { // Only checking for top limit as the parameters are unsigned to guard from negative values
+    return; // If it is out of bounds just return
+  }
+
+  uint16_t index = XYToIndex(x, y); // Figure out what array index the pixel being drawn to is contained within
+
+  black_pixel_buffer_.at(index) &= ~(0b1 << (x % 8)); // Mask off the bit and change it to 0
+  // First expression isolates the black pixel data from the color, then the second part shifts it over into the correct position
+  black_pixel_buffer_.at(index) |= ((value(color) & 0b1) << (x % 8));
+
+  color_pixel_buffer_.at(index) &= ~(0b1 << (x % 8)); // Mask off the bit and change it to 0
+  // First expression isolates the color pixel data and shifts it into bit position 0, then the second part shifts it over into the correct position
+  color_pixel_buffer_.at(index) |= (((value(color) & 0b1) >> 1) << (x % 8));
 }
 
 void ITC::DisplayBuffer() {
   Write(Registers::black_frame);  // Send the register to prepare for the black pixel data
   for (uint16_t current_data_byte = 0; current_data_byte < black_pixel_buffer_.size(); ++current_data_byte) {
-    Write(black_pixel_buffer_.at(current_data_byte));
+    Write(black_pixel_buffer_.at(current_data_byte)); // Go through the entire buffer and write it byte by byte
   }
 
   Write(Registers::color_frame);  // Send the register to prepare for the color pixel data
   for (uint16_t current_data_byte = 0; current_data_byte < color_pixel_buffer_.size(); ++current_data_byte) {
-    Write(color_pixel_buffer_.at(current_data_byte));
+    Write(color_pixel_buffer_.at(current_data_byte)); // Go through the entire buffer and write it byte by byte
   }
 
-  delay(50);
+  delay(50); // TODO: Figure out how short this delay needs to be
 
   Write(Registers::power_on);  // Turn the internal DC to DC converter on
   delay(5);
-  while (BusyPin() != 1)
-    ;
-  Write(Registers::display_refresh);  // Issue the command to move the epaper particles
+  while (BusyPin() != 1); // Wait until the display signals it is ready for the next instructions
+  Write(Registers::display_refresh);  // Issue the command to move the epaper particles to refresh the screen
   delay(5);
-  while (BusyPin() != 1)
-    ;
+  while (BusyPin() != 1); // Wait until the display signals it is ready for the next instructions
   Write(Registers::power_off);  // Turn off the DC to DC converter once the particles are in position
-  while (BusyPin() != 1)
-    ;
+  while (BusyPin() != 1); // Wait until the display signals it is ready for the next instructions
 
-  ResetPin(0);
+  ResetPin(0); // Set the reset pin low as specified
 }
 
 void ITC::FillBuffer(Colors color) {
@@ -68,30 +79,26 @@ void ITC::FillBuffer(Colors color) {
 }
 
 inline uint16_t ITC::XYToIndex(uint16_t x, uint16_t y) {
-  return ((x / 2) + (y * 64));
+  return (x / 8) + (y * (width_ / 8)); // Calculate what byte index in the pixel buffers the selected pixel will reside at
 }
 
-inline uint16_t ITC::XYToPixel(uint16_t x, uint16_t y) {
-  return (x & 0b1) ? (((x / 2) + 1) + (y * 64)) : ((x / 2) + (y * 64));
-}
-
-inline void ITC::ChipSelectPin(bool state) { digitalWrite(chip_select_pin_, state); }
-inline void ITC::CommandDataPin(bool state) { digitalWrite(command_data_pin_, state); }
-inline void ITC::ResetPin(bool state) { digitalWrite(reset_pin_, state); }
-inline bool ITC::BusyPin() { return digitalRead(busy_pin_); }
+inline void ITC::ChipSelectPin(bool state) { digitalWrite(chip_select_pin_, state); } // Short method to set the chip select pin
+inline void ITC::CommandDataPin(bool state) { digitalWrite(command_data_pin_, state); } // Short method to set the command data pin
+inline void ITC::ResetPin(bool state) { digitalWrite(reset_pin_, state); } // Short method to set the reset pin
+inline bool ITC::BusyPin() { return digitalRead(busy_pin_); } // Short method to read the busy pin's state
 
 // SPI handling methods
 // Writes a single 8 bit value to the defined register
 inline void ITC::Write(uint8_t command_value) {
-  ChipSelectPin(0);
-  SPI.transfer(command_value);
+  ChipSelectPin(0); // CS pin must be toggled between each byte written
+  SPI.transfer(command_value); // Write the byte to the bus
   ChipSelectPin(1);
 }
 
 inline void ITC::Write(Registers command_register) {
-  CommandDataPin(0);
-  ChipSelectPin(0);
-  SPI.transfer(static_cast<uint8_t>(command_register));
+  CommandDataPin(0); // As it is a command register being written, toggle the command pin
+  ChipSelectPin(0); // CS pin must be toggled between each byte written
+  SPI.transfer(value(command_register)); // Write the byte to the bus
   ChipSelectPin(1);
   CommandDataPin(1);
 }
--- lib/iTC/PDi_iTC.hpp
+++ lib/iTC/PDi_iTC.hpp
@@ -12,6 +12,11 @@ class ITC {
   //* ════════════════════════════════════════════════════════════════════════════════════════════════════
  public:
   //* ────────────────────────────────────────────────────────────────────────────────────────────────────
+  // The values assigned to each of these pixel color values is important, a common expression used thoughout this library
+  // is "color & 0b1" and "(color & 0b10 >> 1)". The first using 0b1 is used to see if the pixel should be black, hence why
+  // the Colors number for black is 1. The second one using 0b10 checks if the pixel should be a color (red) and then shifts
+  // the result right once to standardize its position like the black pixel code. Note that white is 0 as if neither of the
+  // bits are set, both operations result in 0 which the display interprets as white. Long story short: don't change these.
   enum class Colors : uint8_t {
     white = 0x0,
     black = 0x1,
@@ -68,6 +73,8 @@ class ITC {
     power_off = 0x02
   };
 
+  uint16_t XYToIndex(uint16_t x, uint16_t y);
+
   /**
    * @brief Converts an enum class member into its underlying value.
    * @param enum_class_member
@@ -85,21 +92,18 @@ class ITC {
   const uint8_t panel_settings_second_byte_ = 0x89;
 
 
-  static constexpr uint16_t width_ = 296;
-  static constexpr uint16_t height_ = 152;
+  static constexpr uint16_t width_ = 296; /**< Number of pixels in the X axis of the display */
+  static constexpr uint8_t width_in_bytes_ = 37; // Divide width by 8, might replace with constexpr if possible
+  static constexpr uint16_t height_ = 152; /**< Number of pixels in the Y axis of the display */
+  static constexpr uint16_t pixel_buffer_size_ = 5624; /**< width_ * height_ / 8 to get each buffer size */
 
-  static constexpr uint16_t pixel_buffer_size_ = 5624;
-
-  std::array<uint8_t, pixel_buffer_size_>(black_pixel_buffer_);
-  std::array<uint8_t, pixel_buffer_size_>(color_pixel_buffer_);
+  std::array<uint8_t, pixel_buffer_size_>(black_pixel_buffer_); /**< Used to store pixel buffer 1 (black pixels) */
+  std::array<uint8_t, pixel_buffer_size_>(color_pixel_buffer_); /**< Used to store pixel buffer 2 (color pixels) */
 
   uint8_t chip_select_pin_ = 0;
   uint8_t command_data_pin_ = 0;
   uint8_t reset_pin_ = 0;
   uint8_t busy_pin_ = 0;
-
-  uint16_t XYToIndex(uint16_t x, uint16_t y);
-  uint16_t XYToPixel(uint16_t x, uint16_t y);
 
   void ResetPin(bool state);
   void ChipSelectPin(bool state);
